08e69a3c9dd6b36aec25b0552b9f197c36f6669e
y.mikhin <y.mikhin@fun-box.ru>
2019-11-16 19:59:45 +0300

    Добавление аргументов и опций (Шаг 2).

--- src/gendiff.js
+++ src/gendiff.js
@@ -3,4 +3,6 @@ const program = require('commander');
 export default () => program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format <type>', 'Output format')
   .parse(process.argv);
