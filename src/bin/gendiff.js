0e08c0ba5d1442b6058f9123a116b8798b9b7e2a
y.mikhin <y.mikhin@fun-box.ru>
2019-11-24 16:33:01 +0300

    Добавление основной функции сравнения файлов.

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
 
+import program from 'commander';
 import gendiff from '../gendiff';
 
-gendiff();
+const init = () => program
+  .version('0.1.0')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log(gendiff(firstConfig, secondConfig));
+  })
+  .option('-f, --format <type>', 'Output format')
+  .parse(process.argv);
+
+init();
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,8 +1,37 @@
-const program = require('commander');
+const fs = require('fs');
 
-export default () => program
-  .version('0.1.0')
-  .description('Compares two configuration files and shows a difference.')
-  .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format <type>', 'Output format')
-  .parse(process.argv);
+export default (pathToFile1, pathToFile2) => {
+  const file1 = JSON.parse(fs.readFileSync(pathToFile1).toString());
+  const file2 = JSON.parse(fs.readFileSync(pathToFile2).toString());
+
+  const onlyNewProperties = Object.keys(file2).reduce((acc, property) => {
+    if (!file1[property] || file2[property] !== file1[property]) {
+      return [...acc, {
+        action: 'added',
+        name: property,
+        value: file2[property],
+      }];
+    }
+    return acc;
+  }, []);
+
+  return Object.keys(file1).reduce((acc, property) => {
+    if (!file2[property] || file2[property] !== file1[property]) {
+      return [...acc, {
+        action: 'deleted',
+        name: property,
+        value: file1[property],
+      }];
+    }
+    if (file2[property] === file1[property]) {
+      return [...acc, {
+        action: 'not changed',
+        name: property,
+        value: file1[property],
+      }];
+    }
+    return acc;
+  }, onlyNewProperties)
+  // eslint-disable-next-line no-nested-ternary
+    .sort((a, b) => ((a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)));
+};
