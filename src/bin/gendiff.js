20fa482ade4006ccfdf7c9442e528a685b6ce6da
y.mikhin <y.mikhin@fun-box.ru>
2021-04-15 15:10:57 +0300

    Добавление в справку опции -f.

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,6 +5,7 @@ const program = require('commander');
 const init = () => program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv);
 
 init();
