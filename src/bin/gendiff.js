53efcee20058931e2980a415f4d82ec085513b75
y.mikhin <y.mikhin@tinkoff.ru>
2022-02-05 22:25:41 +0300

    Добавление ссылки на актуальную версию из package.json в бинарнике gendiff.js.

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const packageJSON = require('../../package.json');
 const showDifference = require('../show-difference');
 
 const init = () => program
-  .version('1.0.0')
+  .version(packageJSON.version)
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .arguments('<firstConfig> <secondConfig>')
